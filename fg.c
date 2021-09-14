e02310e5ef0d2d0212d32761b7027fe2f613c27a
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2021-09-14 18:30:16 +0400

    Update fg.c

--- fg.c
+++ fg.c
@@ -1,9 +1,9 @@
-#include "headers.h"
-#include "fg.h"
-#include "execute_cmd.h"
+#include "utils.h"
 
 void fg_implementation(int parts, char *cmd,  char *args[])
 {
+    signal(SIGTTOU, SIG_IGN);
+    setpgid(0, 0);
     pid_t pid = fork();
     if (pid < 0)
     {
@@ -14,10 +14,14 @@ void fg_implementation(int parts, char *cmd,  char *args[])
     {
         int wstatus;
         pid_t ppid = getpid();
+        tcsetpgrp(0, pid);
         waitpid(pid, &wstatus, WUNTRACED);
+        tcsetpgrp(0, ppid);
+        signal(SIGTTOU, SIG_DFL);
     }
     else if (pid == 0)
     {
+        printf("%d \n", getpid());
         execute_cmd(parts, cmd, args);
     }
     return;
