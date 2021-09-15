aa1f4fbc8e9b552a98f4c25c2a56329ed101e13f
nickinack <karthik.viswanathan@research.iiit.ac.in>
2021-09-15 20:49:39 +0400

    file ls

--- ls.c
+++ ls.c
@@ -5,16 +5,19 @@
 void ls_implementation(int parts, char **args)
 {
     char *dirs_to_ls[MINI_SZE];
+    char *file_to_ls[MINI_SZE];
     for (int i = 0; i < MINI_SZE; i++)
     {
         dirs_to_ls[i] = (char *)malloc(MINI_SZE);
+        file_to_ls[i] = (char *)malloc(MINI_SZE);
     }
-        int dirs_num = 0;
+    int dirs_num = 0;
+    int file_num = 0;
     int flag_a = 0;
     int flag_l = 0;
     if (parts == 0)
     {
-        dirs_to_ls[0] = ".";
+        strcpy(dirs_to_ls[0], ".");
         dirs_num = 1;
     }
     for (int i = 0; i < parts; i++)
@@ -38,26 +41,46 @@ void ls_implementation(int parts, char **args)
             {
                 args[i] = home_dir;
             }
-            struct stat dir;
-            if (stat(args[i], &dir) == 0 && S_ISDIR(dir.st_mode))
+            if(access(args[i], F_OK ) == 0 && strcmp(args[i], "..") && strcmp(args[i], ".")) 
             {
-                strcpy(dirs_to_ls[dirs_num], args[i]);
-                dirs_num++;
+                strcpy(file_to_ls[file_num], args[i]);
+                file_num++;
             }
-            else
+            else 
             {
-                printf("ls: cannot access '%s': no such file or directory\n", args[i]);
+                struct stat dir;
+                if (stat(args[i], &dir) == 0 && S_ISDIR(dir.st_mode))
+                {
+                    strcpy(dirs_to_ls[dirs_num], args[i]);
+                    dirs_num++;
+                }
+                else
+                {
+                    printf("ls: cannot access '%s': no such file or directory\n", args[i]);
+                }
             }
         }
     }
-    if (dirs_num == 0 && (flag_a || flag_l))
+    if (dirs_num == 0 && (flag_a || flag_l) && file_num == 0)
     {
-        dirs_to_ls[dirs_num] = ".";
+        strcpy(dirs_to_ls[dirs_num], ".");
         dirs_num++;
+    }
+    for (int i = 0; i < file_num; i++)
+    {
+        if (flag_l != 1)
+        {
+            printf("%s \n\n", file_to_ls[i]);
+        }
+        if (flag_l == 1)
+        {
+            ls_file(file_to_ls[i]);
+            printf("\n");
+        }
     }
     for (int i = 0; i < dirs_num; i++)
     {
-        if (dirs_num > 1)
+        if (dirs_num > 1 || file_num >= 1)
         {
             printf("%s: \n", tilda_add(dirs_to_ls[i]));
         }
@@ -72,7 +95,6 @@ void ls_implementation(int parts, char **args)
             printf("\n\n");
         }
     }
-
     return;
 }
 
@@ -147,6 +169,46 @@ void ls_dir(char *dir, int flag_a, int flag_l)
     free(entry);
     free(folder);
     return;
+}
+
+void ls_file(char *system)
+{
+    struct stat fileStat;
+    struct passwd *usr;
+    struct group *grp;
+    char *date = (char *)malloc(MINI_SZE);
+    if (stat(system, &fileStat) < 0)
+    {
+        printf("not able to access files in the directory \n");
+        return;
+    }
+    usr = getpwuid(fileStat.st_uid);
+    grp = getgrgid(fileStat.st_gid);
+    if (!usr->pw_name || !grp->gr_name)
+    {
+        printf("unable to retrieve user/group \n");
+        return;
+    }
+    printf( (S_ISDIR(fileStat.st_mode))  ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\t%d", fileStat.st_nlink);
+    printf("\t%s", usr->pw_name);
+    printf("\t%s", grp->gr_name);
+    printf("\t%lld", (long long int)fileStat.st_size);
+    strftime(date, 50, "%b  %d  %I: %M", gmtime(&(fileStat.st_ctime)));
+    printf("\t%s", date);
+    printf("\t%s", system);
+    free(system);
+    free(date);
+    printf("\n");
 }
 
 blkcnt_t cnt_blocks(char *dir)
--- ls.h
+++ ls.h
@@ -4,7 +4,9 @@
 #include <grp.h>
 #include <uuid/uuid.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 blkcnt_t cnt_blocks(char *dir);
 void ls_dir(char *dir, int flag_a, int flag_l);
+void ls_file(char *system);
 void ls_implementation(int parts, char **args);
